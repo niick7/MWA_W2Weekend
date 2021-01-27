580021898068f0ef15a0be24f55b56f9fda1bef3
Nhan Vo <niick7@gmail.com>
2021-01-26 19:11:40 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Instructions
+
+1- Add Authentication to the MEAN games application. Similar to what we covered in class.
+
+2- Add Authentication to your MEAN application. 
+
+3- Add Searching ability to your MEAN application.
+
+Yes, you have a few extra days to finish this work. This is to enable you to relax on Saturday (1/23). But I highly recommend you start working on this Sunday (1/24). Keep in mind, next week there are going to be more assignments and if you don't start Sunday you may not have enough time during the week.
---
+++ app.js
@@ -0,0 +1,24 @@
+require("./db/connection");
+
+const express = require("express");
+const app = express();
+const routes = require("./api/routes");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+app.set("port", 3000);
+app.use(function(req, res, next) {
+  console.log(req.method, req.url);
+  next();
+})
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/api", routes);
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
+
+const server = app.listen(app.get("port"), function(){
+  const port = server.address().port;
+  console.log("Listening to port", port);
+})
